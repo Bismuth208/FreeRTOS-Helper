68c11bd0314bfb87eccc1cfeab04dafed74d3cf7
Bismuth208 <bismuth20883@gmail.com>
2020-02-05 22:01:51 +0300

    add support for configSUPPORT_STATIC_ALLOCATION;;

--- FreeRTOS_helper.hpp
+++ FreeRTOS_helper.hpp
@@ -3,14 +3,21 @@
 
 // - - - - - - - - -- - - - - - - - - - - - - - -
 
-#include "Arduino.h"
+#include <Arduino.h>
 
 // - - - - - - - - -- - - - - - - - - - - - - - -
 
+// template<size_t StackSizeWords>
 class Task
 {
 public:
   TaskHandle_t m_xTask = NULL;
+  const uint32_t m_ulStackSizeWords = 0;
+
+#if configSUPPORT_STATIC_ALLOCATION
+  StaticTask_t m_xTaskControlBlock;
+  StackType_t m_xStack[m_ulStackSizeWords]; // FIXME: this is not working in C++ ...
+#endif
 
   Task(TaskFunction_t pxTaskFunc,
     uint32_t ulStackSizeWords = configMINIMAL_STACK_SIZE,
@@ -20,13 +27,29 @@ public:
 #endif
     void * const pvArgs = NULL,
     const char * const pcFuncName = "\0"
-  )
+  ) : m_ulStackSizeWords(ulStackSizeWords)
   {
+#ifdef ESP32
+#if configSUPPORT_STATIC_ALLOCATION
     if (ulPinnedCore > 1) {
-      xTaskCreate( pxTaskFunc, pcFuncName, ulStackSizeWords, pvArgs, uxPriority, &m_xTask );
+      m_xTask = xTaskCreateStaticPinnedToCore( pxTaskFunc, pcFuncName, m_ulStackSizeWords, pvArgs, uxPriority, m_xStack, &m_xTaskControlBlock, (BaseType_t) ulPinnedCore );
     } else {
-      xTaskCreatePinnedToCore( pxTaskFunc, pcFuncName, ulStackSizeWords, pvArgs, uxPriority, &m_xTask, ulPinnedCore );
+      m_xTask = xTaskCreateStatic( pxTaskFunc, pcFuncName, m_ulStackSizeWords, pvArgs, uxPriority, m_xStack, &m_xTaskControlBlock );
     }
+#else
+    if (ulPinnedCore > 1) {
+      xTaskCreatePinnedToCore( pxTaskFunc, pcFuncName, m_ulStackSizeWords, pvArgs, uxPriority, &m_xTask, ulPinnedCore );
+    } else {
+      xTaskCreate( pxTaskFunc, pcFuncName, m_ulStackSizeWords, pvArgs, uxPriority, &m_xTask );
+    }
+#endif // configSUPPORT_STATIC_ALLOCATION
+#else
+#if configSUPPORT_STATIC_ALLOCATION
+    m_xTask = xTaskCreateStatic( pxTaskFunc, pcFuncName, m_ulStackSizeWords, pvArgs, uxPriority, m_xStack, &m_xTaskControlBlock );
+#else
+    xTaskCreate( pxTaskFunc, pcFuncName, m_ulStackSizeWords, pvArgs, uxPriority, &m_xTask );
+#endif
+#endif // ESP32
   }
 
   void stop(void) {
@@ -41,8 +64,10 @@ public:
     xTaskNotifyGive(m_xTask);
   }
 
-  UBaseType_t waitSignal(TickType_t xTicksToWait = portMAX_DELAY) {
-    return ulTaskNotifyTake(pdFALSE, xTicksToWait);
+  void waitSignal(TickType_t xTicksToWait = portMAX_DELAY) {
+    do {
+      // taskYIELD(); // TODO: add something here
+    } while (ulTaskNotifyTake(pdFALSE, xTicksToWait) == 0UL);
   }
 };
 
@@ -55,7 +80,14 @@ public:
   const QueueHandle_t m_xQueueHandler = NULL;
   const size_t m_xQueueSize = 0;
 
-  Queue() : m_xQueueHandler(xQueueCreate(QueueSize, sizeof(T))), m_xQueueSize(QueueSize) {};
+#if configSUPPORT_STATIC_ALLOCATION
+  StaticQueue_t m_xControlBlock;
+  T m_xStorage[m_xQueueSize]; // this is ok, thx to template
+
+  Queue() : m_xQueueHandler( xQueueCreateStatic(QueueSize, sizeof(T), static_cast<uint8_t*>(m_xStorage), &m_xControlBlock) ), m_xQueueSize(QueueSize) {};
+#else
+  Queue() : m_xQueueHandler( xQueueCreate(QueueSize, sizeof(T)) ), m_xQueueSize(QueueSize) {};
+#endif
 
   BaseType_t receive(T *val, TickType_t xTicksToWait = portMAX_DELAY) {
     return xQueueReceive(m_xQueueHandler, val, xTicksToWait);
@@ -81,7 +113,13 @@ class Mutex
 public:
   const SemaphoreHandle_t m_xMutex = NULL;
 
-  Mutex() : m_xMutex (xSemaphoreCreateMutex()) {}
+#if configSUPPORT_STATIC_ALLOCATION
+  StaticSemaphore_t m_xMutexControlBlock;
+
+  Mutex() : m_xMutex ( xSemaphoreCreateMutexStatic(&m_xMutexControlBlock) ) {}
+#else
+  Mutex() : m_xMutex ( xSemaphoreCreateMutex() ) {}
+#endif
 
   BaseType_t lock(TickType_t xTicksToWait = portMAX_DELAY) {
     return xSemaphoreTake(m_xMutex, xTicksToWait);      
@@ -100,7 +138,13 @@ class Counter
 public:
   const SemaphoreHandle_t m_xConunter = NULL;
 
-  Counter() : m_xConunter(xSemaphoreCreateCounting(MaxCount, 0)) {}
+#if configSUPPORT_STATIC_ALLOCATION
+  StaticSemaphore_t m_xSemaphoreControlBlock;
+
+  Counter() : m_xConunter( xSemaphoreCreateCounting(MaxCount, 0, &m_xSemaphoreControlBlock) ) {}
+#else
+  Counter() : m_xConunter( xSemaphoreCreateCounting(MaxCount, 0) ) {}
+#endif
 
   BaseType_t take(TickType_t xTicksToWait = portMAX_DELAY) {
     return xSemaphoreTake(m_xConunter, xTicksToWait);
@@ -120,30 +164,43 @@ public:
 class Timer
 {
 public:
-  TimerHandle_t m_xTimerHandler = NULL;
+  TimerHandle_t m_xTimer = NULL;
+
+#if configSUPPORT_STATIC_ALLOCATION
+  StaticTimer_t m_xTimerControlBlock;
+#endif
 
   Timer( TimerCallbackFunction_t pxCallbackFunction,
     const UBaseType_t uxAutoReload = pdFALSE,
     void *const pvTimerID = NULL,
     const char *const pcTimerName = "\0" )
+#if configSUPPORT_STATIC_ALLOCATION
+  : m_xTimer( xTimerCreateStatic(pcTimerName, 1, uxAutoReload, pvTimerID, pxCallbackFunction, &m_xTimerControlBlock) )
+#else
+  : m_xTimer( xTimerCreate(pcTimerName, 1, uxAutoReload, pvTimerID, pxCallbackFunction) )
+#endif
   {
-    m_xTimerHandler = xTimerCreate(pcTimerName, 1, uxAutoReload, pvTimerID, pxCallbackFunction);
+    // nothing here yet
   };
 
   BaseType_t start(TickType_t xTimerPeriodInMs = 0) {
     TickType_t xTiks = xTimerPeriodInMs / portTICK_PERIOD_MS;
     
-    xTimerChangePeriod(m_xTimerHandler, xTiks, 0);
-    return xTimerStart(m_xTimerHandler, 0);
+    xTimerChangePeriod(m_xTimer, xTiks, 0);
+    return xTimerStart(m_xTimer, 0);
   }
 
   BaseType_t stop() {
-    return xTimerStop(m_xTimerHandler, 0);
+    return xTimerStop(m_xTimer, 0);
+  }
+
+  BaseType_t isActive() {
+    return xTimerIsTimerActive(m_xTimer);
   }
   
 #if 0
   BaseType_t restart(TickType_t xTimerPeriodInMs = 0) {
-    return xTimerReset(m_xTimerHandler, (xTimerPeriodInMs * portTICK_PERIOD_MS));
+    return xTimerReset(m_xTimer, (xTimerPeriodInMs * portTICK_PERIOD_MS));
   }
 #endif
 };
